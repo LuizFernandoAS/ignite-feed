afd3c6b549be9cc3f038375ebfa12211ab5d599d
Luiz Fernando Araujo da Silva <luizfernandoaraujodasilva@hotmail.com>
2023-05-10 13:36:40 -0300

    feat: Estilização do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,30 @@
+import styles from './Post.module.css';
 export function Post(props){
     return(
-        <div>
-            <h2>{props.author}</h2>
-            <span>{props.content}</span>
-        </div>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src='https://github.com/LuizFernandoAS.png' />
+                    <div className={styles.authorInfo}>
+                        <strong>Luiz Fernando</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title="10 de Maio às 13:00h" dateTime="2023-05-10 13:01:30">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="#"> jane.design/doctorcare</a></p>
+
+                <p>
+                    <a href="#">#novoprojeto</a>{' '}
+                    <a>#nlw </a>{' '}
+                    <a>#rocketseat</a>
+                </p>
+            </div>
+        </article>
     )
 }
