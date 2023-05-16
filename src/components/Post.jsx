66ae537cac036a354618e45e56b83045ba9ed63d
Luiz Fernando Araujo da Silva <luizfernandoaraujodasilva@hotmail.com>
2023-05-16 09:07:15 -0300

    Estado nos comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,9 +4,13 @@ import ptBR from 'date-fns/locale/pt-BR'
 import { Avatar } from './Avatar.jsx';
 import { Comment } from './Comment.jsx';
 import styles from './Post.module.css';
-
+import { useState } from 'react';
 
 export function Post({author, publishedAt, content}){
+const [comments, setComments] = useState([
+    1,
+    2])
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",{
         locale: ptBR,
     })
@@ -16,6 +20,12 @@ export function Post({author, publishedAt, content}){
         addSuffix: true,
     })
     
+    function handleCreateNewComment(){
+        event.preventDefault();
+
+        setComments([...comments, comments.length + 1])
+    }
+
     return(
         <article className={styles.post}>
             <header>
@@ -42,7 +52,7 @@ export function Post({author, publishedAt, content}){
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
@@ -53,9 +63,9 @@ export function Post({author, publishedAt, content}){
             </form>
 
             <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
+                {comments.map(comment=>{
+                    return <Comment/>
+                })}
             </div>
         </article>
     )
